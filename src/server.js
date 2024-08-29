1c76342ccfe0cfd9fcdd31a1095daef638e8c74d
handriottine <henrique.andriotti1@gmail.com>
2024-08-29 10:43:52 -0300

    Altera o server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Boas-vindas ao API do ALLBOOKS")
+  console.log("API disponivel atraves da url http://localhost:8000")
 })
